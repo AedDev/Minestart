c9bd3292ed60e63c6330e3e5af7d9a89ed717e5a
Enrico Ludwig <admin@mds-tv.de>
2016-04-01 05:42:06 +0000

    Pushed version to 1.2.1.0 stable

--- minestart.sh
+++ minestart.sh
@@ -5,7 +5,7 @@
 ################################################################################
 #                                                                              #
 # Author:      Enrico Ludwig (Morph)                                           #
-# Version:     1.2.1.0-dev (31. March 2016)                                    #
+# Version:     1.2.1.0     (01. April 2016)                                    #
 # License:     GNU GPL v2 (See: http://www.gnu.org/licenses/gpl-2.0.txt)       #
 # Created:     11. May 2014                                                    #
 # Description: Control your Minecraft Server                                   #
@@ -15,7 +15,7 @@
 ################################################################################
 ###                             APPLICATION                                  ###
 ################################################################################
-VERSION="1.2.1.0-dev"
+VERSION="1.2.1.0"
 SCRIPT_DIR=$(dirname $0)
 
 # Prints info message to shell
